894221f2c7311a11fabd80a5714e481b58bdea4f
Bmerino04 <bmerinomunoz0404@gmail.com>
2024-10-09 15:21:52 -0300

    structure cleaning and add countries list to app.vue

--- taller-consumo-api/src/App.vue
+++ taller-consumo-api/src/App.vue
@@ -1,85 +1,32 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div>
+    <h1>Lista de países</h1>
+    <ul>
+      <li v-for="(dato, index) in datos" :key="index">
+        <h2>{{ dato.country }} ({{ dato.code }})</h2>
+        <div v-for="(poblacion, i) in dato.populationCounts" :key="i">
+          <span>Población {{ i + 1 }}: {{ poblacion.value }}</span>
+        </div>
+      </li>
+    </ul>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+<script setup>
+import { ref, onMounted } from 'vue'
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+const datos = ref([])
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+onMounted(async () => {
+  try {
+    const res = await fetch('https://countriesnow.space/api/v0.1/countries/population')
+    if (!res.ok) {
+      throw new Error('Error al cargar')
+    }
+    const { data } = await res.json()
+    datos.value = data
+  } catch (error) {
+    console.error('Error:', error)
   }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+})
+</script>
